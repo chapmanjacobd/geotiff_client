c3d936d0bddf7de8fe165924b3df9a23f6de6b00
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2021-12-29 13:54:14 -0500

    gitignore index package-lock package favicon App logo HelloWorld main vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-export default {
-  build: {
-    sourcemap: true,
-  }
-}
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+});
