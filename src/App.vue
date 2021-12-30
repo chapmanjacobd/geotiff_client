ce0ce3c6d3e9719e5c6ef06e160f3860a96e9045
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2021-12-30 18:43:49 -0500

    App TheMap main

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang='ts'>
 import TheLayerSelector from './components/TheLayerSelector.vue'
+import TheMap from './components/TheMap.vue';
 
 </script>
 
 <template>
   <TheLayerSelector />
+  <TheMap />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import OpenLayersMap from "vue3-openlayers";
+import "vue3-openlayers/dist/vue3-openlayers.css";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(OpenLayersMap);
+
+app.mount("#app");
